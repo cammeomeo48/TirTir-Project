1523cd256fbe3a057096ac77ef43ca23ecbdf8fd
chuvietcam48 <cee.m48@gmail.com>
2026-01-22 13:41:23 +0700

    feat: Implement dynamic navigation menu with dropdown support; add menu items data structure

--- frontend/tirtir-frontend/src/app/shared/components/header/header.ts
+++ frontend/tirtir-frontend/src/app/shared/components/header/header.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { MENU_ITEMS } from '../../../core/constants/menu.data';
+
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
-
+  menuItems = MENU_ITEMS;
 }
